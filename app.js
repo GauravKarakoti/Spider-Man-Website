6b1f74eb2d57bfb51f46cb3715ee111a89ecb9d5
Gaurav Karakoti <karakotigaurav12@gmail.com>
2024-12-22 22:34:51 +0530

    https://github.com/GauravKarakoti/Spider-Man-Website.git

---
+++ app.js
@@ -0,0 +1,6 @@
+// Add a little interactivity
+document.querySelectorAll('.extra-images').forEach(img => {
+    img.addEventListener('click', () => {
+        alert('This is an image of Spider-Man history!');
+    });
+});
